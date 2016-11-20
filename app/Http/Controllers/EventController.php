d6dd6e7898cae5fc4616bd40ec87a7eff016be83
alice <rowlinest90@gmail.com>
2016-11-20 22:36:24 +0300

    Добавлен календарь

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\EventModel;
+class EventController extends Controller
+{
+    public function index(){
+        
+        
+    }
+    
+    public function calendar(){
+        $EventModels = EventModel::all();
+        foreach($EventModels as $event){
+            $event_to_calendar[] = \Calendar::event(
+                $event->name,
+                false,
+                $event->start,
+                $event->end,
+                $event->id
+            );
+        }
+
+        $calendar = \Calendar::addEvents($event_to_calendar);
+
+
+        return view('calendar/index', compact('calendar'));
+    }
+    
+    
+    
+}
---
+++ resources/views/calendar/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('styles')
+    {{  HTML::style('css/calendar.css') }}
+@endsection
+
+
+@section('content')
+    {!! $calendar->calendar() !!}
+    {!! $calendar->script() !!}
+
+@endsection
+
+@section('scripts')
+    {{  HTML::script('js/calendar.js') }}
+@endsection
